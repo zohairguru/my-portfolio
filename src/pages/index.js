55765c920d179ca688f97dc2feb9aefd50383322
zohairguru <diasolarpower@gmail.com>
2023-04-30 18:50:43 +0500

    edit home file

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
+      
         <Head>
         <title>Zohair Faruqi's Portfolio</title>
         <meta name="description" content="Joel's Portfolio" />
