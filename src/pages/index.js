a96b05c1251d69c0369ac9cfcd152cd3750e3803
zohairguru <diasolarpower@gmail.com>
2023-04-30 18:03:34 +0500

    new changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Skills from '@/components/Skills'
 import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
 import Head from 'next/head';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -18,6 +19,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
+       
       <Hero />
       <About />
       <Skills />
