9135729f0df5fa37344a42f84b08eaa353dd1bcb
zohairguru <diasolarpower@gmail.com>
2023-04-30 11:02:54 +0500

    add portfolio files

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+
+import { Inter } from 'next/font/google'
+import Hero from '@/components/Hero'
+import About from '@/components/About'
+import Skills from '@/components/Skills'
+import Projects from '@/components/Projects'
+import Contact from '@/components/Contact'
+import Head from 'next/head';
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+        <Head>
+        <title>Zohair Faruqi's Portfolio</title>
+        <meta name="description" content="Joel's Portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      </div>
+    </>
+  )
+}
